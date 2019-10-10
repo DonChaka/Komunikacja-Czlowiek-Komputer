dca9443d26ce3b3b2a625973bef71139b9d55218
DonChaka <DonChaka98@gmail.com>
2019-10-10 13:43:16 +0200

    Enigma dzialajaca (chyba) w konsoli

--- Projekt/src/Main/Main.java
+++ Projekt/src/Main/Main.java
@@ -1,18 +1,38 @@
 package Main;
 
+import Encryption.Enigma;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application
+{
+
+    private static Enigma enigma;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //FXMLLoader loader = new FXMLLoader();
+        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+        //Controller controller = loader.getController();
+
+        enigma = new Enigma(1,2,3);
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root, 600, 450);
+        scene.setOnKeyPressed(event -> {
+            char x = event.getText().charAt(0);
+            System.out.println("Pressed: " + x);
+            System.out.println(enigma.encrypt(x));
+        });
+
+        scene.setOnKeyReleased(event -> {
+            enigma.tickRotors();
+            enigma.printRotors();
+        });
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
