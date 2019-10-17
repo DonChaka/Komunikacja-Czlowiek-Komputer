a43f24aa4a1b62e252193f5b21068596a559388d
DonChaka <DonChaka98@gmail.com>
2019-10-17 20:14:30 +0200

    negatywne ticki

--- Projekt/src/com/Main/Main.java
+++ Projekt/src/com/Main/Main.java
@@ -19,18 +19,9 @@ public class Main extends Application
 
         enigma = new Enigma(1,2,3);
 
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Enigma");
         Scene scene = new Scene(root, 530, 902);
-        scene.setOnKeyPressed(event -> {
-            char x = event.getText().charAt(0);
-            enigma.tickRotors();
-            System.out.println("Pressed: " + x);
-            System.out.println(enigma.encrypt(x));
-        });
 
-        scene.setOnKeyReleased(event -> {
-            enigma.printRotors();
-        });
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
